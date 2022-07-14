4a98ac88944f1cdbd89917728b65a20939efc089
Exempti0n <sj990927@gmail.com>
2022-07-14 14:02:34 +0900

    Initial Commit

---
+++ MyAsset/MyAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetApp.swift
+//  MyAsset
+//
+//  Created by 오승준 on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
